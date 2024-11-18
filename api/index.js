c0035addc308a9b2413a2c1042614a0e75e08323
Eduardo Ribeiro <eribeiro@icapitalnetwork.com>
2024-11-18 17:50:26 +0000

    feat(API): :bug: correct API and SQLite DB.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
-const fastify = require('fastify');
-const sqlite3 = require('sqlite3').verbose();
-
-const app = fastify();
-const port = process.env.PORT || 4001;
+import fastify from 'fastify';
+import sqlite3 from 'sqlite3';
 
 const db = new sqlite3.Database('./db/stats.db', (err) => {
   if (err) {
@@ -11,91 +8,98 @@ const db = new sqlite3.Database('./db/stats.db', (err) => {
   console.log('Connected to the statistics database.');
 });
 
-app.use(express.json());
+const statsApi = async () => {
+  const app = fastify();
 
-// GET all products
-app.get('/packages', (req, res) => {
-  db.all('SELECT * FROM packages', (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal server error');
-    } else {
-      res.send(rows);
-    }
+  app.get('/', (req, reply) => {
+    reply.send('change me to see updates, fastify!~~');
   });
-});
 
-// GET single product by ID
-app.get('/packages/:id', (req, res) => {
-  const { id } = req.params;
-  db.get('SELECT * FROM packages WHERE id = ?', [id], (err, row) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal server error');
-    } else if (!row) {
-      res.status(404).send('Product not found');
-    } else {
-      res.send(row);
-    }
-  });
-});
-
-// POST new product
-app.post('/packages', (req, res) => {
-  const { name, price } = req.body;
-  if (!name || !price) {
-    res.status(400).send('Name and price are required');
-  } else {
-    const sql = 'INSERT INTO packages(name) VALUES (?, ?)';
-    db.run(sql, [name, price], function(err) {
+  // GET all products
+  app.get('/packages', (req, res) => {
+    db.all('SELECT * FROM packages', (err, rows) => {
       if (err) {
         console.error(err.message);
         res.status(500).send('Internal server error');
       } else {
-        const id = this.lastID;
-        res.status(201).send({ id, name, price });
+        res.send(rows);
       }
     });
-  }
-});
+  });
 
-// PUT update product by ID
-app.put('/packages/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, price } = req.body;
-  if (!name || !price) {
-    res.status(400).send('Name and price are required');
-  } else {
-    const sql = 'UPDATE packages SET name = ?, price = ? WHERE id = ?';
-    db.run(sql, [name, price, id], function(err) {
+  // GET single product by ID
+  app.get('/packages/:id', (req, res) => {
+    const { id } = req.params;
+    db.get('SELECT * FROM packages WHERE id = ?', [id], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send('Internal server error');
+      } else if (!row) {
+        res.status(404).send('Product not found');
+      } else {
+        res.send(row);
+      }
+    });
+  });
+
+  // POST new product
+  app.post('/packages', (req, res) => {
+    const { name, price } = req.body;
+    if (!name || !price) {
+      res.status(400).send('Name and price are required');
+    } else {
+      const sql = 'INSERT INTO packages(name) VALUES (?, ?)';
+      db.run(sql, [name, price], function (err) {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal server error');
+        } else {
+          const id = this.lastID;
+          res.status(201).send({ id, name, price });
+        }
+      });
+    }
+  });
+
+  // PUT update product by ID
+  app.put('/packages/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, price } = req.body;
+    if (!name || !price) {
+      res.status(400).send('Name and price are required');
+    } else {
+      const sql = 'UPDATE packages SET name = ?, price = ? WHERE id = ?';
+      db.run(sql, [name, price, id], function (err) {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal server error');
+        } else if (this.changes === 0) {
+          res.status(404).send('Product not found');
+        } else {
+          res.status(200).send({ id, name, price });
+        }
+      });
+    }
+  });
+
+  // DELETE product by ID
+  app.delete('/packages/:id', (req, res) => {
+    const { id } = req.params;
+    db.run('DELETE FROM packages WHERE id = ?', [id], function (err) {
       if (err) {
         console.error(err.message);
         res.status(500).send('Internal server error');
       } else if (this.changes === 0) {
         res.status(404).send('Product not found');
       } else {
-        res.status(200).send({ id, name, price });
+        res.status(204).send();
       }
     });
-  }
-});
-
-// DELETE product by ID
-app.delete('/packages/:id', (req, res) => {
-  const { id } = req.params;
-  db.run('DELETE FROM packages WHERE id = ?', [id], function(err) {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal server error');
-    } else if (this.changes === 0) {
-      res.status(404).send('Product not found');
-    } else {
-      res.status(204).send();
-    }
   });
-});
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
-});
+  app.listen(4001, () => { console.log('Server listen on port 4001') });
+
+  return app;
+};
+
+export const viteNodeApp = statsApi();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePluginNode } from 'vite-plugin-node'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginNode({adapter: 'express', appPath: './api'})],
-})
+  plugins: [react()]
+});
